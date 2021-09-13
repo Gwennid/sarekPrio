0e4c6dcc235cb1a7fd377041ec17a6d9382e47ae
Tobias Osterlund <xsteto@medair1.medair.lcl>
2021-09-13 12:32:51 +0200

    added sarek commands

--- sarekPrio.sh
+++ sarekPrio.sh
@@ -6,9 +6,13 @@
 #$ -pe mpi 40
 #$ -l excl=1
 
-tsv=tsv/TL-2784-SARK027-B.tsv
+tsv=/medstore/Illumina_Tobias/cv/tsv/TL-2784-SARK012.tsv
+outdirname=$(basename $tsv |sed s/'.tsv'//g)
+echo $outdirname
 genome=GRCh38
 genome_base=/medstore/Illumina_Tobias/db/GRCh38/
 
 ./scripts/run_trimming.sh $tsv
-
+nextflow run main.nf --sample $tsv --step mapping --genome $genome --genome_base $genome_base -profile docker --outDir $outdirname --noReports
+nextflow run somaticVC.nf --outDir $outdirname --genome $genome --genome_base $genome_base -profile docker --tools mutect2,strelka,freebayes --noReports
+nextflow run runMultiQC.nf --outDir $outdirname -profile docker
