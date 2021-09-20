f8d45b65f2c76fdf5aecb4b273c9a38d2ae30399
Tobias Osterlund <xsteto@medair1.medair.lcl>
2021-09-20 13:04:38 +0200

    updated sarekPrio.sh

--- sarekPrio.sh
+++ sarekPrio.sh
@@ -5,14 +5,24 @@
 #$ -q batch.q
 #$ -pe mpi 40
 #$ -l excl=1
+module load nextflow
+module load singularity
+Sarek_installation_path=/apps/bio/software/sarek/
 
+#Setup
 tsv=/medstore/Illumina_Tobias/cv/tsv/TL-2784-SARK012.tsv
 outdirname=$(basename $tsv |sed s/'.tsv'//g)
 echo $outdirname
-genome=GRCh38
-genome_base=/medstore/Illumina_Tobias/db/GRCh38/
+genome=GRCh37
+genome_base=/medstore/Illumina_Tobias/db/GRCh37/
 
-./scripts/run_trimming.sh $tsv
-nextflow run main.nf --sample $tsv --step mapping --genome $genome --genome_base $genome_base -profile docker --outDir $outdirname --noReports
-nextflow run somaticVC.nf --outDir $outdirname --genome $genome --genome_base $genome_base -profile docker --tools mutect2,strelka,freebayes --noReports
-nextflow run runMultiQC.nf --outDir $outdirname -profile docker
+#Run preprocessing
+./scripts/run_trimming.sh $tsv $outdirname
+newtsvname=$OUTPUT_DIRECTORY/trimmed/"$( basename $TSV_FILE)"
+
+
+#Run Sarek pipeline
+nextflow run $Sarek_installation_path/main.nf --sample $newtsvname --step mapping --genome $genome --genome_base $genome_base -profile singularityPath --outDir $outdirname --noReports
+nextflow run $Sarek_installation_path/somaticVC.nf --outDir $outdirname --genome $genome --genome_base $genome_base -profile singualrityPath --tools mutect2,strelka,freebayes --noReports
+nextflow run $Sarek_installation_path/runMultiQC.nf --outDir $outdirname -profile singularityPath
+
